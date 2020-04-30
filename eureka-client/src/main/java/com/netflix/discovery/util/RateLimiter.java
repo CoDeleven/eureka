622f804386155e9d344bb5d28e6d4fac516e1a5c
codeleven <codelevex@gmail.com>
2020-04-30 23:52:26 +0800

    限流器阅读，已经80%理解了，就是怎么转换为自己的语言，还有如何影响流量的需要再研究研究

--- eureka-client/src/main/java/com/netflix/discovery/util/RateLimiter.java
+++ eureka-client/src/main/java/com/netflix/discovery/util/RateLimiter.java
@@ -25,32 +25,40 @@ import java.util.concurrent.atomic.AtomicLong;
  * <ul>
  * <li>
  *     burst size - maximum number of requests allowed into the system as a burst
+ *     桶里允许放入的最大令牌数量
  * </li>
  * <li>
  *     average rate - expected number of requests per second (RateLimiters using MINUTES is also supported)
+ *     令牌刷新间隔时间。如果设定的单位是分钟，那么就是每隔一分钟放入averageRate个令牌；
+ *     如果设定的单位是秒，那么就是每隔一秒钟放入averageRate个令牌
  * </li>
  * </ul>
  *
  * @author Tomasz Bak
  */
 public class RateLimiter {
-
+    // 记时单位
     private final long rateToMsConversion;
-
+    // 消耗的令牌数量
     private final AtomicInteger consumedTokens = new AtomicInteger();
+    // 上一次填充令牌的时间
     private final AtomicLong lastRefillTime = new AtomicLong(0);
 
     @Deprecated
     public RateLimiter() {
         this(TimeUnit.SECONDS);
     }
-
+    // 设置填充令牌速率的单位
     public RateLimiter(TimeUnit averageRateUnit) {
         switch (averageRateUnit) {
+            // 如果单位是秒
             case SECONDS:
+                // 设置基本时间单位
                 rateToMsConversion = 1000;
                 break;
+            // 如果单位是分钟欧冠
             case MINUTES:
+                // 设置基本事件单位
                 rateToMsConversion = 60 * 1000;
                 break;
             default:
@@ -58,32 +66,68 @@ public class RateLimiter {
         }
     }
 
+    /**
+     * 尝试获取一个令牌
+     * @param burstSize 该桶允许容纳的最大令牌数量
+     * @param averageRate 生成新令牌的频率
+     * @return
+     */
     public boolean acquire(int burstSize, long averageRate) {
         return acquire(burstSize, averageRate, System.currentTimeMillis());
     }
 
+    /**
+     * 尝试获取一个令牌
+     * @param burstSize 该桶允许容纳的最大令牌数量
+     * @param averageRate 生成新令牌的频率
+     * @param currentTimeMillis 当前时间
+     * @return
+     */
     public boolean acquire(int burstSize, long averageRate, long currentTimeMillis) {
+        // 如果这两个参数小于0，就让流量全都通过
         if (burstSize <= 0 || averageRate <= 0) { // Instead of throwing exception, we just let all the traffic go
             return true;
         }
-
+        // 填充令牌
         refillToken(burstSize, averageRate, currentTimeMillis);
         return consumeToken(burstSize);
     }
 
+    /**
+     * 填充令牌
+     * @param burstSize 该桶允许容纳的最大令牌数量
+     * @param averageRate 生成新令牌的频率
+     * @param currentTimeMillis 当前时间
+     */
     private void refillToken(int burstSize, long averageRate, long currentTimeMillis) {
+        // 获取上一次填充的时间
         long refillTime = lastRefillTime.get();
+        // 获取此时 和 之前填充时间的时间差
         long timeDelta = currentTimeMillis - refillTime;
-
+        // 时间差（单位毫秒） * 生成速率（单位个/UNIT）/ UNIT * 1000
+        // 代入一个例子，当前单位是分钟，时间差是30秒，生成速率是60（即每分钟60个）
+        // 结果 30 * 1000 * 60 / 60 * 1000
+        // 这段时间产生了30个新的令牌
         long newTokens = timeDelta * averageRate / rateToMsConversion;
+        // 新的令牌数量大于0
         if (newTokens > 0) {
+            // 计算新的填充令牌的时间
             long newRefillTime = refillTime == 0
                     ? currentTimeMillis
                     : refillTime + newTokens * rateToMsConversion / averageRate;
+            // CAS
             if (lastRefillTime.compareAndSet(refillTime, newRefillTime)) {
                 while (true) {
+                    // 获取当前已经消耗的令牌数量
                     int currentLevel = consumedTokens.get();
+                    // 比较 当前消耗的令牌数量 和 新生成的令牌数量，取最小值
+                    // 为什么取最小值？因为这一步只想获取当前已经消耗的令牌数量
+                    // 消耗的令牌数量不能超过桶的最大值
+                    // 如果消耗的令牌数量超过了此时指定的桶内最大令牌数（爆满）
+                    // 就以此时桶内最大令牌数为基准来计算
                     int adjustedLevel = Math.min(currentLevel, burstSize); // In case burstSize decreased
+                    // 如果adjustedLevel - newTokens < 0，说明新生成的令牌数量够用（并抵消了之前的已经被取走的令牌）
+                    // 如果adjustedLevel - newTokens > 0，说明新生成的令牌数量被用掉了一部分
                     int newLevel = (int) Math.max(0, adjustedLevel - newTokens);
                     if (consumedTokens.compareAndSet(currentLevel, newLevel)) {
                         return;
@@ -93,18 +137,29 @@ public class RateLimiter {
         }
     }
 
+    /**
+     * 消耗一个令牌
+     * @param burstSize 此时允许桶内最大令牌数
+     * @return
+     */
     private boolean consumeToken(int burstSize) {
         while (true) {
+            // 获取此时消耗的令牌数
             int currentLevel = consumedTokens.get();
+            // 如果已经消耗的令牌数 大于等于 桶内可容纳最大令牌数，就返回false
             if (currentLevel >= burstSize) {
                 return false;
             }
+            // 如果允许再拿令牌，就增加一个消耗的令牌数
             if (consumedTokens.compareAndSet(currentLevel, currentLevel + 1)) {
                 return true;
             }
         }
     }
 
+    /**
+     * 重置限流器
+     */
     public void reset() {
         consumedTokens.set(0);
         lastRefillTime.set(0);
