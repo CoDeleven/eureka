e562c9a5c70db53657b8f8adcf27846a98319bec
CoDeleven <codelevex@gmail.com>
2020-05-06 17:39:55 +0800

    限流器

--- eureka-client/src/main/java/com/netflix/discovery/util/RateLimiter.java
+++ eureka-client/src/main/java/com/netflix/discovery/util/RateLimiter.java
@@ -56,9 +56,9 @@ public class RateLimiter {
                 // 设置基本时间单位
                 rateToMsConversion = 1000;
                 break;
-            // 如果单位是分钟欧冠
+            // 如果单位是分钟
             case MINUTES:
-                // 设置基本事件单位
+                // 设置基本时间单位
                 rateToMsConversion = 60 * 1000;
                 break;
             default:
@@ -70,7 +70,7 @@ public class RateLimiter {
      * 尝试获取一个令牌
      * @param burstSize 该桶允许容纳的最大令牌数量
      * @param averageRate 生成新令牌的频率
-     * @return
+     * @return 是否成功
      */
     public boolean acquire(int burstSize, long averageRate) {
         return acquire(burstSize, averageRate, System.currentTimeMillis());
@@ -81,7 +81,7 @@ public class RateLimiter {
      * @param burstSize 该桶允许容纳的最大令牌数量
      * @param averageRate 生成新令牌的频率
      * @param currentTimeMillis 当前时间
-     * @return
+     * @return 是否成功
      */
     public boolean acquire(int burstSize, long averageRate, long currentTimeMillis) {
         // 如果这两个参数小于0，就让流量全都通过
@@ -90,11 +90,13 @@ public class RateLimiter {
         }
         // 填充令牌
         refillToken(burstSize, averageRate, currentTimeMillis);
+        // 消耗令牌
         return consumeToken(burstSize);
     }
 
     /**
      * 填充令牌
+     *
      * @param burstSize 该桶允许容纳的最大令牌数量
      * @param averageRate 生成新令牌的频率
      * @param currentTimeMillis 当前时间
@@ -114,21 +116,22 @@ public class RateLimiter {
             // 计算新的填充令牌的时间
             long newRefillTime = refillTime == 0
                     ? currentTimeMillis
+                    // FIXME 为什么不直接获取timeDelta？？
                     : refillTime + newTokens * rateToMsConversion / averageRate;
-            // CAS
+            // CAS，保证只有一个线程处理
             if (lastRefillTime.compareAndSet(refillTime, newRefillTime)) {
                 while (true) {
                     // 获取当前已经消耗的令牌数量
                     int currentLevel = consumedTokens.get();
-                    // 比较 当前消耗的令牌数量 和 新生成的令牌数量，取最小值
-                    // 为什么取最小值？因为这一步只想获取当前已经消耗的令牌数量
-                    // 消耗的令牌数量不能超过桶的最大值
-                    // 如果消耗的令牌数量超过了此时指定的桶内最大令牌数（爆满）
-                    // 就以此时桶内最大令牌数为基准来计算
-                    int adjustedLevel = Math.min(currentLevel, burstSize); // In case burstSize decreased
-                    // 如果adjustedLevel - newTokens < 0，说明新生成的令牌数量够用（并抵消了之前的已经被取走的令牌）
-                    // 如果adjustedLevel - newTokens > 0，说明新生成的令牌数量被用掉了一部分
+                    // 当前已消耗的令牌数量。
+                    // 这里取最小值的原因是为了消耗的令牌数量超过桶内最大值时，取桶的上限；
+                    // 不足桶内最大值时，取当前已消耗令牌数量；
+                    // 这一步可以通过远程配置中心控制瞬时流量并发大小，只要外部设置的burstSize比原先的大即可
+                    int adjustedLevel = Math.min(currentLevel, burstSize);
+                    // 这一步是填充令牌，如果adjustedLevel - newTokens < 0，说明此次填充能全部填充满（消耗的令牌数量归零）
+                    // adjustedLevel - newTokens > 0，说明新生成的令牌数量不够用，不能完全填满
                     int newLevel = (int) Math.max(0, adjustedLevel - newTokens);
+                    // CAS，防止竞争
                     if (consumedTokens.compareAndSet(currentLevel, newLevel)) {
                         return;
                     }
